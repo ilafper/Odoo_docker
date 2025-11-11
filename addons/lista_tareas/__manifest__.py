fd65016b232a533939d3ba769b74fa338229c260
ilafper <ivanaalfaya@gmail.com>
2025-11-11 18:10:19 +0100

    iiuiuiu

---
+++ addons/lista_tareas/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "lista_tareas",
+    'summary': "Sencilla lista de tareas",
+    'description': """
+        Módulo para gestionar tareas simples con prioridad y estado.
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Productivity',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+    'security/ir.model.access.csv',
+    'views/views.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+}
---
+++ addons/lista_tareas/models/models.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+class ListaTareas(models.Model):
+    _name = 'lista_tareas.lista_tareas'   # Debe coincidir exactamente con la vista
+    _description = 'Modelo de tareas'
+
+    tarea = fields.Char(string='Tarea')
+    prioridad = fields.Integer(string='Prioridad')
+    urgente = fields.Boolean(string='Urgente', compute='_compute_urgente', store=True)
+    realizada = fields.Boolean(string='Realizada')
+
+    @api.depends('prioridad')
+    def _compute_urgente(self):
+        for record in self:
+            record.urgente = record.prioridad > 10
