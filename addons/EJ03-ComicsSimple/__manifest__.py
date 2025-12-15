d3f4fd64f920f27036bc8edced7904d2d7a183b4
ilafper <ivanaalfaya@gmail.com>
2025-12-15 16:35:33 +0100

    asdad

--- addons/EJ03-ComicsSimple/__manifest__.py
+++ addons/EJ03-ComicsSimple/__manifest__.py
@@ -25,7 +25,7 @@ Gestor de bibliotecas (Version Simple)
         'security/ir.model.access.csv',
         #Cargamos la vista de la biblioteca de comics
         'views/biblioteca_comic.xml',
-        #
+        #vista de socio
         'views/socio.xml',
     ],
     # Fichero con data de demo si se inicializa la base de datos con "demo data" (No incluido en ejemplo)
