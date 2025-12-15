28eb8d23eeb291d7a2240fd19021c5686113fec7
ilafper <ivanaalfaya@gmail.com>
2025-12-15 15:37:07 +0100

    cambio para que aparezca el nombre del de la tarea

--- addons/lista_tareas/models/models.py
+++ addons/lista_tareas/models/models.py
@@ -3,12 +3,13 @@ from odoo import models, fields, api
 class ListaTareas(models.Model):
     _name = 'lista_tareas.lista_tareas'
     _description = 'Modelo de tareas'
-
+    
+    _rec_name = 'tarea'
     tarea = fields.Char(string='Tarea')
     prioridad = fields.Integer(string='Prioridad')
     urgente = fields.Boolean(string='Urgente', compute='_compute_urgente', store=True)
     realizada = fields.Boolean(string='Realizada')
-    
+
     deadline = fields.Date(string='Fecha Límite')
 
     @api.depends('prioridad')
